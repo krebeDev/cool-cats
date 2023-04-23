8cbe74d167e1063ee3aa6cea0624494c8bba71d3
Solomon Ekrebe <krebedev@gmail.com>
2023-04-23 17:07:54 +0100

    creates home-page shell

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Home = (): JSX.Element => {
+  return (
+    <section>
+      <h1>Welcome to the home of cool cats!</h1>
+    </section>
+  )
+}
+
+export default Home
