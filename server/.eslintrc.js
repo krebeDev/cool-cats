d89a9fc74fbf73b6cc3a173d609d01b5268be4d9
Solomon Ekrebe <krebedev@gmail.com>
2023-04-24 15:39:59 +0100

    updates ecma vesrion to string type

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['plugin:@typescript-eslint/recommended'],
   parserOptions: {
-    ecmaVersion: latest,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
