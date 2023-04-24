c7f2809520d96738858123d17343bd14b6ca5125
Solomon Ekrebe <krebedev@gmail.com>
2023-04-24 21:48:42 +0100

    implements cat-details page

--- client/src/pages/Cat.tsx
+++ client/src/pages/Cat.tsx
@@ -1,7 +1,51 @@
 import * as React from 'react'
+import { useLocation } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_API_URL } from '../constants'
+import CatDetails from '../components/CatDetails'
+import { Link } from 'react-router-dom'
 
-const Cat = (): JSX.Element => {
-  return <div>I'm an awesome cat!</div>
+const CatDetailsPage = (): JSX.Element => {
+  const [catDetails, setCatDetails] = React.useState<any>({})
+  const [loading, setLoading] = React.useState<boolean>(true)
+  const [error, setError] = React.useState<string>('')
+
+  const location = useLocation()
+  const catId = location.pathname.split('/').pop()
+
+  React.useEffect(() => {
+    const fetchCatsBreeds = async (): Promise<void> => {
+      try {
+        const { data } = await axios.get(`${BASE_API_URL}/cat/${catId}`)
+        setCatDetails(data)
+      } catch (error) {
+        setError('An unexpected error occured.')
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchCatsBreeds()
+  }, [])
+
+  const catBreed = catDetails?.breeds ? catDetails.breeds[0].id : null
+
+  return (
+    <>
+      {loading && <div>Loading...</div>}
+      {!loading && !error && (
+        <section>
+          <div>
+            <Link to='/' state={{ breed: catBreed }}>
+              ← Back
+            </Link>
+          </div>
+          <CatDetails cat={catDetails} />
+        </section>
+      )}
+      {error && <p>{error}</p>}
+    </>
+  )
 }
 
-export default Cat
+export default CatDetailsPage
