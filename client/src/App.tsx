b2e16a24480fa980b3621e27d8577f7400f51b1e
Solomon Ekrebe <krebedev@gmail.com>
2023-04-25 18:49:17 +0100

    manage app state with react-context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,64 @@ import Layout from './components/Layout'
 import NotFoundPage from './pages/NotFound'
 import CatDetailsPage from './pages/Cat'
 import HomePage from './pages/Home'
+import CatsContext from './context'
+import { CatSummary, CatXContextType } from './types/index.type'
 
 const App = (): JSX.Element => {
+  const [catsState, setCatsState] = React.useState<CatXContextType>({
+    cats: [],
+    nextBatchUrl: null,
+    catBreeds: [],
+    selectedBreed: '',
+    loadedBreed: '',
+  })
+
+  const saveCats = React.useCallback(
+    (
+      cats: CatSummary[],
+      breed: string,
+      nextBatch: string | null,
+      sync: boolean
+    ): void => {
+      setCatsState((currentValue: CatXContextType) => ({
+        ...currentValue,
+        cats: sync ? [...currentValue.cats, ...cats] : cats,
+        loadedBreed: breed,
+        nextBatchUrl: nextBatch,
+        // TODO: Filter off duplicates
+      }))
+    },
+    []
+  )
+
+  const saveCatsBreeds = React.useCallback((breeds: any[]): void => {
+    setCatsState((currentValue: CatXContextType) => ({
+      ...currentValue,
+      catBreeds: breeds,
+    }))
+  }, [])
+
+  const saveSelectedBreed = React.useCallback((breed: string): void => {
+    setCatsState((currentValue: CatXContextType) => ({
+      ...currentValue,
+      selectedBreed: breed,
+    }))
+  }, [])
+
+  const contextValue = React.useMemo(
+    () => ({
+      catsState,
+      saveCats,
+      saveCatsBreeds,
+      saveSelectedBreed,
+    }),
+    [catsState, saveCats, saveCatsBreeds, saveSelectedBreed]
+  )
+
   const router = createBrowserRouter([
     {
       path: '/',
       element: <HomePage />,
-      // TODO: Add errorElement prop
     },
     {
       path: 'cats/:catId',
@@ -23,10 +74,11 @@ const App = (): JSX.Element => {
   ])
 
   return (
-    // Add App Context here
-    <Layout>
-      <RouterProvider router={router} />
-    </Layout>
+    <CatsContext.Provider value={contextValue}>
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
+    </CatsContext.Provider>
   )
 }
 
