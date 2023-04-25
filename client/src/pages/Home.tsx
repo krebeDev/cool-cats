c91233b13572d32810c8f793cd716244bf77b60f
Solomon Ekrebe <krebedev@gmail.com>
2023-04-25 18:52:04 +0100

    applies context data

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,38 +1,27 @@
 import * as React from 'react'
-import { useLocation } from 'react-router-dom'
-import axios from 'axios'
 import BreedForm from '../components/BreedForm'
-import { BASE_API_URL } from '../constants'
-import { CatSummary } from './../types/index.type'
+import { ALERT_MESSAGE } from '../constants'
 import CatPhoto from '../components/CatPhoto'
+import CatsContext from '../context'
+import PageShell from '../components/PageShell'
+import _axios from '../config/axios'
 
 const HomePage = (): JSX.Element => {
-  const [cats, setCats] = React.useState<CatSummary[] | []>([])
-  const [catBreeds, setCatBreeds] = React.useState<any[]>([])
-  const [nextBatchUrl, setNextBatchUrl] = React.useState<string | null>(null)
   const [loading, setLoading] = React.useState<boolean>(true)
-  const [errors, setErrors] = React.useState<string>('')
+  const [error, setError] = React.useState<string>('')
 
-  const location = useLocation()
+  const { catsState, saveCats, saveCatsBreeds } = React.useContext(CatsContext)
 
   const fetchCats = async (
     next: string | null,
-    breed?: string | null,
+    breed: string = '',
     sync: boolean = false
   ): Promise<void> => {
-    const apiEndpoint =
-      next && !breed
-        ? next
-        : breed
-        ? `${BASE_API_URL}/breeds/${breed}`
-        : `${BASE_API_URL}`
+    const apiEndpoint = sync ? next : breed ? `/breeds/${breed}` : `/`
 
     try {
-      const { data } = await axios.get(apiEndpoint)
-      // TODO: Filter off duplicates
-      setCats((cats) => (sync ? [...cats, ...data.result] : data.result))
-      setNextBatchUrl(data.next)
-      // Handle end of results case
+      const { data } = await _axios.get(apiEndpoint as string)
+      saveCats(data.result, breed, data.next, sync)
     } catch (error) {
       throw error
     }
@@ -40,20 +29,27 @@ const HomePage = (): JSX.Element => {
 
   const fetchCatsBreeds = async (): Promise<void> => {
     try {
-      const { data } = await axios.get(`${BASE_API_URL}/breeds`)
-      setCatBreeds(data)
+      const { data } = await _axios.get(`/breeds`)
+      saveCatsBreeds(data)
     } catch (error) {
       throw error
     }
   }
 
   const initPage = async () => {
-    const breed = location.state?.breed // change to context
     try {
-      await fetchCats(null, breed)
-      await fetchCatsBreeds()
+      if (
+        !catsState.cats.length ||
+        catsState.selectedBreed !== catsState.loadedBreed
+      ) {
+        await fetchCats(null, catsState.selectedBreed)
+      }
+
+      if (!catsState.catBreeds.length) {
+        await fetchCatsBreeds()
+      }
     } catch (error) {
-      setErrors('An unexpected error occured.')
+      setError(ALERT_MESSAGE)
     } finally {
       setLoading(false)
     }
@@ -64,7 +60,7 @@ const HomePage = (): JSX.Element => {
       setLoading(true)
       await fetchCats(null, breed)
     } catch (error) {
-      setErrors('An unexpected error occured.')
+      setError(ALERT_MESSAGE)
     } finally {
       setLoading(false)
     }
@@ -77,34 +73,30 @@ const HomePage = (): JSX.Element => {
   }, [])
 
   return (
-    <>
-      {loading && <div>Loading...</div>}
-      {!loading && !errors && (
-        <section>
-          <h1>Welcome to the home of cool cats!</h1>
-          <BreedForm onSelect={fetchCatsByBreed} breeds={catBreeds} />
-          <div>
-            <ul className='row'>
-              {cats.map((cat) => (
-                <CatPhoto key={cat.id} cat={cat} />
-              ))}
-            </ul>
-            {nextBatchUrl && (
-              <div>
-                <button
-                  type='button'
-                  onClick={() => fetchCats(nextBatchUrl, null, true)}
-                  disabled={loading}
-                >
-                  Load More
-                </button>
-              </div>
-            )}
-          </div>
-        </section>
-      )}
-      {errors && <p>{errors}</p>}
-    </>
+    <PageShell loading={loading} error={error}>
+      <section>
+        <h1>Welcome to the home of cool cats!</h1>
+        <BreedForm onSelect={fetchCatsByBreed} />
+        <div>
+          <ul className='row'>
+            {catsState.cats.map((cat) => (
+              <CatPhoto key={cat.id} cat={cat} />
+            ))}
+          </ul>
+          {catsState.nextBatchUrl && (
+            <div>
+              <button
+                type='button'
+                onClick={() => fetchCats(catsState.nextBatchUrl, '', true)}
+                disabled={loading}
+              >
+                Load More
+              </button>
+            </div>
+          )}
+        </div>
+      </section>
+    </PageShell>
   )
 }
 
