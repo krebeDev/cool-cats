6a20085b078cca342617b1bae0f8359f12a5dc40
Solomon Ekrebe <krebedev@gmail.com>
2023-04-25 23:39:33 +0100

    reads base-api-url from env file

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,3 @@
-// export const BASE_API_URL = process.env.BASE_API_URL
-export const BASE_API_URL = 'http://localhost:5000/api/v1/cats'
+export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL
 export const ALERT_MESSAGE =
   'Apologies but we could not load new cats for you at this time! Miau!'
