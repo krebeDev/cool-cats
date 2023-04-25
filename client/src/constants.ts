7b3720fc6c6b429b4bd7a08986bc251c602dbad0
Solomon Ekrebe <krebedev@gmail.com>
2023-04-25 18:49:38 +0100

    creates alert-message constant

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,4 @@
 // export const BASE_API_URL = process.env.BASE_API_URL
 export const BASE_API_URL = 'http://localhost:5000/api/v1/cats'
+export const ALERT_MESSAGE =
+  'Apologies but we could not load new cats for you at this time! Miau!'
